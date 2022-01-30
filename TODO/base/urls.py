83b10ecc91f5eb3d4ad20276a5cafd7b2baa160f
AKXAT <akshat.aksharma@gmail.com>
2022-01-30 17:16:27 +0530

    added the class based view and setup the HTML response page

--- TODO/base/urls.py
+++ TODO/base/urls.py
@@ -1,6 +1,6 @@
 import imp
 from django.urls import path
-from . import views 
-
+from .views import TaskList
 urlpatterns = [
+    path('',TaskList.as_view(),name='tasklist')
 ]
--- TODO/base/views.py
+++ TODO/base/views.py
@@ -1,3 +1,9 @@
+from .models import Task
+from django.http import request
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
 # Create your views here.
+
+class TaskList(ListView):
+    model = Task
