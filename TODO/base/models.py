d7734d3021bdb97a1d0dbe793a1f081362f78e6d
AKXAT <akshat.aksharma@gmail.com>
2022-01-30 16:39:28 +0530

    Made the models and migrations

--- TODO/base/models.py
+++ TODO/base/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
+from tkinter import CASCADE
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    title = models.CharField(max_length=300)
+    status = models.BooleanField(default=False) 
