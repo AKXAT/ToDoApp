f0535f35ab087eb174dd543b578196dc9e9e9104
AKXAT <akshat.aksharma@gmail.com>
2022-01-30 23:08:34 +0530

    Added options for login and logout with restrictions

--- TODO/base/urls.py
+++ TODO/base/urls.py
@@ -1,6 +1,9 @@
 import imp
 from django.urls import path
-from .views import TaskList
+from .views import Login, TaskList
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
+    path('login/',Login.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(next_page = 'login'),name='logout'),
     path('',TaskList.as_view(),name='tasklist')
 ]
--- TODO/base/views.py
+++ TODO/base/views.py
@@ -1,9 +1,20 @@
+from django.forms import models
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from .models import Task
-from django.http import request
-from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+class Login(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True 
 
-class TaskList(ListView):
+    def get_success_url(self) -> str:
+        return reverse_lazy('tasklist')
+
+
+class TaskList(LoginRequiredMixin , ListView):
     model = Task
